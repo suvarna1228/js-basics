8f3d5466eb02d86cd207c37963dab57bbe7edf0d
suvarna1228 <suvu1228@gmail.com>
2024-09-23 12:04:46 +0530

    started dsa in javascript and learned about object

---
+++ index.js
@@ -0,0 +1,16 @@
+const obj ={
+    name:'nethul',
+    age:22,
+    'key-three':true,
+    saymyname:function(){
+        console.log(this.neame)
+    }
+}
+obj.hobby ='football'
+delete obj.hobby
+console.log(obj.name);
+console.log(obj['age']);
+console.log(obj['key-three']);
+console.log(obj);
+obj.saymyname()
+// .key(),.values(),.eentries()
