b1e0a95026e234099f111930f5dcdc650bce592f
suvarna1228 <suvu1228@gmail.com>
2024-10-05 14:22:13 +0530

    preappend

--- linked.js
+++ linked.js
@@ -1,21 +1,39 @@
-class node {
-    constructor(value){
+class Node {
+    constructor(value) {
         this.value = value;
         this.next = null;
     }
 }
-class  linked_list {
+
+class LinkedList {
     constructor() {
-      this.head =null;
-      this.size =0;  
+        this.head = null;
+        this.size = 0;
     }
-    isEmpty(){
+
+    isEmpty() {
         return this.size === 0;
     }
-    isSize(){
-        return this.size
+
+    getSize() {
+        return this.size;
+    }
+
+    preappend(value) {
+        const node = new Node(value); // Corrected reference to Node
+        if (this.isEmpty()) {
+            this.head = node;
+        } else {
+            node.next = this.head;
+            this.head = node;
+        }
+        this.size++;
     }
 }
-const list = new linked_list()
-console.log('List is Empty?',list.isEmpty());
-console.log('list size', list.isSize());
+
+const list = new LinkedList();
+console.log('List is Empty?', list.isEmpty());
+console.log('List size:', list.getSize());
+list.preappend(40);
+console.log('List is Empty?', list.isEmpty());
+console.log('List size:', list.getSize());
