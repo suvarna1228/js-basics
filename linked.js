6fa936b8b2b906c7e860fc0abcf5fc9592a2cfbb
suvarna1228 <suvu1228@gmail.com>
2024-10-06 16:17:41 +0530

    print

--- linked.js
+++ linked.js
@@ -29,11 +29,28 @@ class LinkedList {
         }
         this.size++;
     }
+    print(){
+      if(this.isEmpty()){
+        console.log('List is Empty')
+      }  
+      else{
+        let curr = this.head;
+        let listValues = '';
+        while(curr){
+            listValues +=`${curr.value}`;
+            curr = curr.next; 
+        }
+        console.log(listValues);
+      }
+    }
 }
 
 const list = new LinkedList();
 console.log('List is Empty?', list.isEmpty());
 console.log('List size:', list.getSize());
+list.print();
 list.preappend(40);
+list.print();
 console.log('List is Empty?', list.isEmpty());
 console.log('List size:', list.getSize());
+list.print();
